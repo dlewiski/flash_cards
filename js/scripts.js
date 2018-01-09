6d0f02f83bbfebdba0ab9521f3519d45c5c5e8f0
david lewis and roger yee <david@>
2018-01-09 13:28:05 -0800

    add definitions to the terms and allows them to be toggled.

---
+++ js/scripts.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+
+  $(".clickable#term1").click(function() {
+    $(".definition#term1").toggle();
+  });
+
+  $(".clickable#term2").click(function() {
+    $(".definition#term2").toggle();
+  });
+
+  $(".clickable#term3").click(function() {
+    $(".definition#term3").toggle();
+  });
+
+  $(".clickable#term4").click(function() {
+    $(".definition#term4").toggle();
+  });
+
+  $(".clickable#term5").click(function() {
+    $(".definition#term5").toggle();
+  });
+
+  $(".clickable#term6").click(function() {
+    $(".definition#term6").toggle();
+  });
+
+  $(".clickable#term7").click(function() {
+    $(".definition#term7").toggle();
+  });
+
+  $(".clickable#term8").click(function() {
+    $(".definition#term8").toggle();
+  });
+
+  $(".clickable#term9").click(function() {
+    $(".definition#term9").toggle();
+  });
+
+  $(".clickable#term10").click(function() {
+    $(".definition#term10").toggle();
+  });
+
+  $(".clickable#term11").click(function() {
+    $(".definition#term11").toggle();
+  });
+
+  $(".clickable#term12").click(function() {
+    $(".definition#term12").toggle();
+  });
+
+  $(".clickable#term13").click(function() {
+    $(".definition#term13").toggle();
+  });
+
+  $(".clickable#term14").click(function() {
+    $(".definition#term14").toggle();
+  });
+
+  $(".clickable#term15").click(function() {
+    $(".definition#term15").toggle();
+  });
+
+  $(".clickable#term16").click(function() {
+    $(".definition#term16").toggle();
+  });
+
+  $(".clickable#term17").click(function() {
+    $(".definition#term17").toggle();
+  });
+
+  $(".clickable#term18").click(function() {
+    $(".definition#term18").toggle();
+  });
+
+  $(".clickable#term19").click(function() {
+    $(".definition#term19").toggle();
+  });
+
+});
